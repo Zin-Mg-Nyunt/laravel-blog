cf3aa30adce6adf29cbd4f3ab7b8866223647c4b
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2025-04-20 02:27:33 +0800

    adding css style for blogs and blog detail

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -7,17 +7,19 @@
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
-    <article>
-        <h1><a href="/blog">First Blog</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi a, corrupti exercitationem unde quidem culpa magnam sequi officia officiis id natus adipisci, atque, amet quo eligendi maxime facere itaque consequatur. Doloribus, autem ratione earum nisi non nesciunt dolor corrupti rerum provident, quia obcaecati aut neque velit culpa, in facilis recusandae perferendis illum cumque eius ut laboriosam. Laudantium mollitia molestiae ullam, ut aspernatur quia cum sit repellendus inventore officiis quos ipsum corporis debitis rerum minus porro cupiditate enim itaque vitae fuga incidunt ducimus eius sint repellat. Veritatis maiores eius deleniti tempore modi laborum qui fuga. Iste a voluptas omnis excepturi est?</p>
-    </article>
-    <article>
-        <h1><a href="/blog">Second Blog</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi a, corrupti exercitationem unde quidem culpa magnam sequi officia officiis id natus adipisci, atque, amet quo eligendi maxime facere itaque consequatur. Doloribus, autem ratione earum nisi non nesciunt dolor corrupti rerum provident, quia obcaecati aut neque velit culpa, in facilis recusandae perferendis illum cumque eius ut laboriosam. Laudantium mollitia molestiae ullam, ut aspernatur quia cum sit repellendus inventore officiis quos ipsum corporis debitis rerum minus porro cupiditate enim itaque vitae fuga incidunt ducimus eius sint repellat. Veritatis maiores eius deleniti tempore modi laborum qui fuga. Iste a voluptas omnis excepturi est?</p>
-    </article>
-    <article>
-        <h1><a href="/blog">Third Blog</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi a, corrupti exercitationem unde quidem culpa magnam sequi officia officiis id natus adipisci, atque, amet quo eligendi maxime facere itaque consequatur. Doloribus, autem ratione earum nisi non nesciunt dolor corrupti rerum provident, quia obcaecati aut neque velit culpa, in facilis recusandae perferendis illum cumque eius ut laboriosam. Laudantium mollitia molestiae ullam, ut aspernatur quia cum sit repellendus inventore officiis quos ipsum corporis debitis rerum minus porro cupiditate enim itaque vitae fuga incidunt ducimus eius sint repellat. Veritatis maiores eius deleniti tempore modi laborum qui fuga. Iste a voluptas omnis excepturi est?</p>
-    </article>
+    <div class="blog-list">
+        <div class="blog-card">
+            <h2><a href="/blog">First Blog</a></h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur harum esse, dolorum incidunt mollitia nisi obcaecati! Obcaecati placeat nostrum facilis dolor magni, dolores repudiandae, nam, tempore amet modi ex veniam sed voluptatibus incidunt aut sapiente natus provident magnam mollitia similique assumenda nisi voluptates libero! Pariatur ea eum culpa, labore doloremque, quis iure laborum ipsam est eaque molestias nesciunt veritatis ducimus, sunt voluptatum sapiente maxime tenetur accusantium doloribus amet obcaecati alias minus! Consectetur dolorem, minus similique explicabo placeat cumque corrupti et. Velit exercitationem quas deleniti ad harum alias saepe a corrupti odit facere? Magni velit consectetur quia dolor quo, ad doloremque.</p>
+        </div>
+        <div class="blog-card">
+            <h2><a href="/blog">Second Blog</a></h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur harum esse, dolorum incidunt mollitia nisi obcaecati! Obcaecati placeat nostrum facilis dolor magni, dolores repudiandae, nam, tempore amet modi ex veniam sed voluptatibus incidunt aut sapiente natus provident magnam mollitia similique assumenda nisi voluptates libero! Pariatur ea eum culpa, labore doloremque, quis iure laborum ipsam est eaque molestias nesciunt veritatis ducimus, sunt voluptatum sapiente maxime tenetur accusantium doloribus amet obcaecati alias minus! Consectetur dolorem, minus similique explicabo placeat cumque corrupti et. Velit exercitationem quas deleniti ad harum alias saepe a corrupti odit facere? Magni velit consectetur quia dolor quo, ad doloremque.</p>
+        </div>
+        <div class="blog-card">
+            <h2><a href="/blog">Third Blog</a></h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur harum esse, dolorum incidunt mollitia nisi obcaecati! Obcaecati placeat nostrum facilis dolor magni, dolores repudiandae, nam, tempore amet modi ex veniam sed voluptatibus incidunt aut sapiente natus provident magnam mollitia similique assumenda nisi voluptates libero! Pariatur ea eum culpa, labore doloremque, quis iure laborum ipsam est eaque molestias nesciunt veritatis ducimus, sunt voluptatum sapiente maxime tenetur accusantium doloribus amet obcaecati alias minus! Consectetur dolorem, minus similique explicabo placeat cumque corrupti et. Velit exercitationem quas deleniti ad harum alias saepe a corrupti odit facere? Magni velit consectetur quia dolor quo, ad doloremque.</p>
+        </div>
+    </div>
 </body>
 </html>
