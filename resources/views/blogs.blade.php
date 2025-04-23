e5ff4838eb890b5244c60511beb92332145878b0
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2025-04-24 01:39:58 +0800

    create dynamically blogs page by using yamlFrontMatter composer package

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -21,8 +21,19 @@
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur harum esse, dolorum incidunt mollitia nisi obcaecati! Obcaecati placeat nostrum facilis dolor magni, dolores repudiandae, nam, tempore amet modi ex veniam sed voluptatibus incidunt aut sapiente natus provident magnam mollitia similique assumenda nisi voluptates libero! Pariatur ea eum culpa, labore doloremque, quis iure laborum ipsam est eaque molestias nesciunt veritatis ducimus, sunt voluptatum sapiente maxime tenetur accusantium doloribus amet obcaecati alias minus! Consectetur dolorem, minus similique explicabo placeat cumque corrupti et. Velit exercitationem quas deleniti ad harum alias saepe a corrupti odit facere? Magni velit consectetur quia dolor quo, ad doloremque.</p>
         </div>
     </div> --}}
-    <?php foreach ($blogs as $blog) :?>
-    <?= $blog;?>
-    <?php endforeach ?>
+    <div class="blog-list">
+        <?php foreach ($blogs as $blog) :?>
+        <div class="blog-card">
+            <h2>
+                <a href="/blogs/<?= $blog->slug?>">
+                    <?= $blog->title?>
+                </a>
+            </h2>
+            <p>
+                <?= $blog->intro?>
+            </p>
+        </div>
+        <?php endforeach; ?>
+    </div>
 </body>
 </html>
