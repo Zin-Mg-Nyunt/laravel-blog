d52738e419c26e1c6e7303764d37dd72c976a031
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2025-04-23 22:37:39 +0800

    using cache

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('blogs');
 });
-Route::get('/blogs/{blog}',function($fileName){
-    $path = __DIR__."/../public/blogs/$fileName.html"; // file path
+Route::get('/blogs/{blog}',function($slug){
+    $path = __DIR__."/../resources/blogs/$slug.html"; // file path
+    if (!file_exists($path)) {
+        abort(404);
+    }
+    $blog=cache()->remember('blogs.$slug', now()->addMinutes(2), function () use($path) {
+        var_dump('first time');
+        return file_get_contents($path);
+    });
     return view('blog',[
-        "blog"=>file_get_contents($path)
+        "blog"=>$blog
     ]);
-});
+})->where('blog','[A-z\d\-_]+');
