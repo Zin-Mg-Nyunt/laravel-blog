be0060fec8faf3e95c969369c744ad4ac3c58cb3
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2025-04-24 23:46:33 +0800

    blade layout system (yield and section)

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,20 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-    <div class="blog-detail-container">
-        <a href="/">&larr; Back to all blogs</a>
-        <h2>
-            {{$blog->title}}
-        </h2>
-        <p>
-            {!!$blog->body!!}
-        </p>
-    </div>
-</body>
-</html>
+@extends('layout')
+@section('title')
+    <title>
+        {{$blog->title}}
+    </title>
+@endsection
+@section('content')    
+<div class="blog-detail-container">
+    <a href="/">&larr; Back to all blogs</a>
+    <h2>
+        {{$blog->title}}
+    </h2>
+    <p>
+        {!!$blog->body!!}
+    </p>
+</div>
+@endsection
--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+@extends('layout')
+@section('title')
+    <title>All Blogs</title>
+@endsection
+@section('content') 
     <div class="blog-list">
         @foreach ($blogs as $blog) 
             <div class="blog-card">
@@ -24,5 +20,4 @@
             </div>
         @endforeach
     </div>
-</body>
-</html>
+@endsection
