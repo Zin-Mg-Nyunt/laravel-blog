90d1da7bbb671ff1c01ce98340b98883aceb00a0
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2025-05-04 01:18:45 +0800

    logical grouping

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -18,9 +18,11 @@ class Blog extends Model
         return $this->belongsTo(Author::class);
     }
     public function scopeFilter($query,$filter){
-        $query->when($filter['search'],function($query,$search){
-            $query=$query->where('title','LIKE','%'.$search.'%')
-                        ->orWhere('body','LIKE','%'.$search.'%');
+        $query->when($filter['search']??false,function($query,$search){
+            $query->where(function($query) use($search){
+                $query->where('title','LIKE','%'.$search.'%')
+                ->orWhere('body','LIKE','%'.$search.'%');
+            });
         });
         return $query;
     }
