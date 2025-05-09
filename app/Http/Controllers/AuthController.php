22ae8cbb32d345e9ae55358f342cc961094c5c74
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2025-05-10 00:50:28 +0800

    validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,9 +7,15 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function create(){
-        return view('auth.create');
+        return view('register.create');
     }
     public function store(){
-        dd(request()->all());
+        request()->validate([
+            'name'=>'required|min:3|max:255',
+            'userName'=>'required|min:3|max:255',
+            'email'=>'required|email',
+            'password'=>'required|min:8'
+        ]);
+        dd('success');
     }
 }
---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,44 @@
+<x-layout>
+    <div class="container">
+      <div class="row">
+        <div class="col-5 mx-auto my-5">
+          <h3 class="text-primary font-extrabold text-center">Register Form</h3>
+          <div class="card p-4 my-3 shadow-sm">
+            <form action="" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Name</label>
+                    <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{old('name')}}">
+                    @error('name')
+                        <p class="text-danger">{{$message}}</p>
+                    @enderror
+                </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Username</label>
+                    <input type="text" name="userName" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{old('userName')}}">
+                    @error('userName')
+                        <p class="text-danger">{{$message}}</p>
+                    @enderror
+                  </div>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Email address</label>
+                  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{old('email')}}">
+                  @error('email')
+                      <p class="text-danger">{{$message}}</p>
+                  @enderror
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Password</label>
+                  <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                  @error('password')
+                      <p class="text-danger">{{$message}}</p>
+                  @enderror
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+</x-layout>
